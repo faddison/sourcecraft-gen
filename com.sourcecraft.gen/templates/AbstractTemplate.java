79a203913a8b6fe2369f651a0ecacdd87f987312
fraser addison <fraseraddison@gmail.com>
2013-11-01 04:58:17 -0700

    added factory classes and implemented abstract template

--- com.sourcecraft.gen/templates/AbstractTemplate.java
+++ com.sourcecraft.gen/templates/AbstractTemplate.java
@@ -1,7 +1,23 @@
 package templates;
 
+import designer.AbstractDesigner;
+import metrics.AbstractMetrics;
+import parser.AbstractParser;
+import planner.AbstractPlanner;
+
 public abstract class AbstractTemplate {
 
-	
+	private AbstractParser parser;
+	private AbstractMetrics metrics;
+	private AbstractDesigner designer;
+	private AbstractPlanner planner;
 
+	public AbstractTemplate()
+	{
+	}
+	
+	public void run()
+	{
+		//planner.plan(designer.design(parser.parse(metrics)));
+	}
 }
